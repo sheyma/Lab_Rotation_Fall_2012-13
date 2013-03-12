e24bbf09b00f9addf3352d4565b245479d932444
Şeyma Bayrak <s.bayrak.89@hotmail.com>
2013-03-12 22:59:27 +0100

    use binofit to plot candle sticks

--- pcorrect.m
+++ pcorrect.m
@@ -5,7 +5,11 @@ load('/home/sheyma/devel/Lab_Rotation_Fall_2012-13/Hysteresis.3D/Hysteresis.Opti
 
 k=length(Block);
 
+
 Lifetime=[1 2 3 4 8 16];
+N=k*(numel(Block{1}.Correct)/numel(Lifetime));
+
+
 P_cor=zeros(length(Lifetime),k);
 P_correct=zeros(size(Lifetime));
 
@@ -13,17 +17,33 @@ for j=1:k
   for i=1:numel(Lifetime)
     index=find(Block{j}.LifetimeInFrames==Lifetime(i));
     c=Block{j}.Correct(index);
-    P_cor(i,j)=sum(c)/length(c);
+    P_cor(i,j)=sum(c);
   end
 end
 
 
 for i=1:numel(Lifetime)
-P_correct(i)=sum(P_cor(i,:))/length(P_cor(i,:));
+    P_correct(i)=sum(P_cor(i,:));
+    
 end
 
 
-P_correct
+[phit, pci]=binofit(P_correct,N);
+
+
+
+hold on
+plot(Lifetime,phit,'--o','MarkerSize',8);
+axis([0 17 0 1])
+for j=1:numel(Lifetime)
+    plot(Lifetime(j),linspace(pci(j,1),pci(j,2)));
+end
+
+
+
+hold off
+
+
 
 % plot(Lifetime,P_correct)
 
