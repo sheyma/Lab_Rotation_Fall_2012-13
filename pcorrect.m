1ed95c240381a464aabfab304b08f0671994e534
Şeyma Bayrak <s.bayrak.89@hotmail.com>
2013-03-14 17:33:03 +0100

    additional comments

--- pcorrect.m
+++ pcorrect.m
@@ -1,9 +1,11 @@
 clear all;
 close all;
+% P_correct (==P_t) versus LifetimeinFrames
 
+% Loading data from observer : Sheyma Bayrak
 load('/Users/seyma/GIT/Lab_Rotation_Fall_2012-13/Hysteresis.3D/Hysteresis.OptimizeLifetime/Results/sheyma.mat');
 
-
+% How manx cells of data I have from the experiment
 k=length(Block);
 
 
@@ -11,34 +13,31 @@ Lifetime=[1 2 3 4 8 16];
 N=k*(numel(Block{1}.Correct)/numel(Lifetime));
 
 
-P_cor=zeros(length(Lifetime),k);
-N_correct=zeros(size(Lifetime));
+N_cor=zeros(length(Lifetime),k);
+N_cor_tot=zeros(size(Lifetime));
 
 for j=1:k
   for i=1:numel(Lifetime)
     index=find(Block{j}.LifetimeInFrames==Lifetime(i));
     c=Block{j}.Correct(index);
-    P_cor(i,j)=sum(c);
+    N_cor(i,j)=sum(c);
   end
 end
 
 
 for i=1:numel(Lifetime)
-    N_correct(i)=sum(P_cor(i,:));
-    
+N_cor_tot(i)=sum(N_cor(i,:));
 end
 
 
 %P_t can be negative, it should be avoided for binofit function
 
-P_t=abs(2*(N_correct/N)-1);
+P_t=abs(2*(N_cor_tot/N)-1);
+ 
+ 
+[phit, pci]=binofit(P_t*N,N);
 
-
-[phit, pci]=binofit(P_t*N,N)
-
-
-
-
+figure;
 hold on
 plot((Lifetime),phit,'--o','MarkerSize',8);
  axis([0 17 0 1])
@@ -48,7 +47,6 @@ end
 hold off
 xlabel('Lifetime in Frames','Fontsize',16);
 ylabel('P_t','Fontsize',16);
-
-% plot(Lifetime,P_correct)
+title('Perceprion in Fraction as Lifetime of Dots','Fontsize',16);
 
     
